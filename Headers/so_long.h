726d0441db3eaf4d76da583ea49e6a892b3ad183
Kari_ma <121245611+KariHab@users.noreply.github.com>
2023-05-15 00:20:00 -0400

    Add files via upload

---
+++ Headers/so_long.h
@@ -0,0 +1,16 @@
+
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "MLX42/include/MLX42/MLX42.h"
+
+
+#define WIDTH 256
+#define HEIGHT 256
+
+
+#endif
---
+++ tryitfirst.c
@@ -0,0 +1,12 @@
+
+// #include "so_long.h"
+
+// /*make the mlx works and pop a sprite random
+// create a map
+//     parsing map
+// check event
+// */
+// int main ()
+// {
+//     return(0);
+// }
